0630125e174aa886b9b76e43bb8218317e2a63ae
mynameajeff <yungcoder@protonmail.com>
2017-06-25 14:33:31 +0100

    Improvements to main.py;
    Added commenting functionality, cleaned up quite a few lines of code, bug-fixing etc.

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys, msvcrt
-
-class toy:
-    class SyntaxError(Exception):
-        pass
-    class ValueError(Exception):
-        pass
+import sys, msvcrt, error
 
 class interpreter:
     def __init__(self, fl):
@@ -20,17 +14,16 @@ class interpreter:
         self.flag_directaout = None
 
         self.flag_skip = None
-
         self.skipby = 0
+
         self.ifln = open(fl).readlines()
         #print(self.ifln) #debug statement, gives all data 4 file
-        self.enum = enumerate(self.ifln)
 
         try:
-            if self.ifln[0] not in ('START\n','start\n') or self.ifln[len(self.ifln)-1] not in ('END','end'):
-                raise toy.SyntaxError("No START or END statement passed in code")
+            if 'START' not in self.ifln[0][:5].upper() or 'END' not in self.ifln[len(self.ifln)-1][:5].upper():
+                raise error.SyntaxError("No START or END statement passed in code")
         except IndexError:
-            raise toy.SyntaxError("No START and END statement passed in code")
+            raise error.SyntaxError("No START and END statement passed in code")
 
         self.main_loop()
 
@@ -38,32 +31,38 @@ class interpreter:
         if self.flag_store:
             i = 0
             while self.imem[i] != 0: i+=1
-            self.imem[i] = self.char_data
-            print("imemory: stored %s at index %s." % (self.char_data, i))
+            self.imem[i] = self.char_data.split("~")[0]
+            print("imemory: stored %s at index %s." % (self.char_data.split("~")[0], i))
             self.flag_store = False
             return 0
 
         if self.flag_out:
             if self.flag_directout:
-                print('direct: input was "%s".' % self.i[5:-1])
+                try: 
+                    print('direct: input was "%s"' % self.i[5:-1].split("~")[0])
+                except ValueError:
+                    raise error.ValueError("Cannot convert %s to integer, is invalid." % self.i[5:-1])
+
                 self.flag_directout = False
                 self.flag_out = False
                 return 0
 
-            i = self.imem[int(self.char_data)]
-            print('imemory: at index %s there is "%s".' % (self.char_data, i))
+            i = self.imem[int(self.char_data.replace("~",""))]
+            print('imemory: at index %s there is "%s".' % (self.char_data.replace("~",""), i))
             self.flag_out = False
             return 0
 
         if self.flag_aout:
             if self.flag_directaout:
 
-                try: print('ascii output: %s' % chr(int(self.i[6:-1])))
+                try: 
+                    print('ascii output: %s' % chr(int(self.i[6:-1].split("~")[0].replace("~",""))))
                 except ValueError:
-                    raise toy.ValueError("Cannot convert %s to integer, is invalid." % self.i[6:-1])
+                    raise error.ValueError("Cannot convert %s to integer, is invalid." % self.i[6:-1])
+
                 self.flag_directaout = False
                 self.flag_skip = True
-                self.skipby = 1
+                while self.i[self.skipby] != "\n": self.skipby+=1
                 self.flag_aout = False
                 return 0
 
@@ -72,7 +71,7 @@ class interpreter:
             self.flag_aout = False
             return 0
 
-        print("Encountered Num:",self.char_data)
+        print("Encountered Num:",self.char_data.split("~")[0])
 
     def encountered(self):
         try: 
@@ -80,27 +79,36 @@ class interpreter:
             self.flag_skip = True
         except ValueError:
             if self.flag_out:
-                raise toy.ValueError("Cannot convert character data to imem index int.")
-                return 0
+                raise error.ValueError("Cannot convert character data to imem index int.")
 
-            print("Encountered Char:",self.char_data)
+            if self.char_data != " ":
+                print("Encountered Char:",self.char_data)
             return 0
         var1 = 1
-        while self.ifln[self.x][self.pos_char+var1] != "\n":
-            self.char_data+=self.ifln[self.x][self.pos_char+var1]
+        while self.i[self.pos_char+var1] not in ("\n"," "):
+            self.char_data+=self.i[self.pos_char+var1]
+
+            if "~" in self.char_data:
+                break
+
             try: float(self.char_data)
             except ValueError:
-                raise toy.ValueError("Cannot convert character data to Float")
+                raise error.ValueError("Cannot convert character data to Float")
             var1+=1
         self.skipby += (var1-1)
 
         self.flag_handler()
 
     def chars(self): #numbers, etc
-        for self.pos_char,self.char_data in enumerate(self.ifln[self.x]):
+        for self.pos_char,self.char_data in enumerate(self.i):
 
             if self.char_data == '\n':
                 #print("newline!")
+                continue
+
+            if self.char_data == "~":
+                self.flag_skip = True
+                self.skipby += len(self.i.split("~")[1])-1
                 continue
 
             if self.flag_skip and self.skipby != 0:
@@ -110,29 +118,30 @@ class interpreter:
             elif self.flag_skip and self.skipby == 0:
                 self.flag_skip = False
 
-            if not self.flag_aout: self.encountered()
+            if not self.flag_aout:
+                self.encountered()
             else: self.flag_handler()
 
     def full_words(self): #keyword handler
-        if self.i in ('START\n','start\n'):
+        if self.i[:5] in ('START','start'):
             if self.flag_start:
-                raise toy.SyntaxError("More than one START statement is present.")
+                raise error.SyntaxError("More than one START statement is present.")
             print("beginning!")
             self.flag_start = True
             self.flag_skip = True
             self.skipby = 5
 
-        elif self.i in ('END','end'):
+        elif self.i[:3] in ('END','end'):
             print("ending...")
             msvcrt.getch()
             sys.exit()
 
-        elif "STORE" == self.i[:5] or "store" == self.i[:5]:
+        elif self.i[:5] in ("STORE","store"):
             self.flag_skip = True
             self.flag_store = True
             self.skipby = 6
 
-        elif "OUT" == self.i[:3] or "out" == self.i[:3]:
+        elif self.i[:3] in ("OUT","out"):
             self.flag_skip = True
             self.flag_out = True
             self.skipby = 4
@@ -140,7 +149,7 @@ class interpreter:
                 self.flag_directout = True
                 self.skipby+=1
 
-        elif "AOUT" == self.i[:4] or "aout" == self.i[:4]:
+        elif self.i[:4] in ("AOUT","aout"):
             self.flag_skip = True
             self.flag_aout = True
             self.skipby = 5
@@ -149,12 +158,11 @@ class interpreter:
                 self.skipby+=1
 
     def main_loop(self):
-        for self.x,self.i in self.enum:
+        for self.x,self.i in enumerate(self.ifln):
             self.full_words()
-            if self.flag_start: 
-                self.chars()
+            self.chars()
 
-dfile = "./runf.toy"
+dfile = "./runf.toye"
 
 if __name__ == "__main__":
     if 'idlelib.run' not in sys.modules:
