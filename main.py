21da9db5f6e98ed449c1d49c42f1c6cc161438f2
mynameajeff <yungcoder@protonmail.com>
2017-07-01 16:16:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,57 +6,30 @@ class interpreter:
         self.var_list = []
 
         self.flag_start = None
+
         self.flag_const = None
+        self.flag_decl = None
 
         self.flag_out = None
         self.flag_imemout = None
         self.flag_directout = None
 
         self.flag_aout = None #ascii
+        self.flag_imemaout = None
         self.flag_directaout = None
-
-        self.flag_decl = None
 
         self.flag_skip = None
         self.skipby = 0
-
-        self.flag_enc = True
 
         self.ifln = open(fl).readlines()
         #print(self.ifln) #debug statement, gives all data 4 file
 
         self.main_loop()
 
-    def char_data_handler(self):
-
-        if self.char_data in (" ","$","i"):
-            return 0
-
-        if self.flag_out:
-            raise error.ValueError("Cannot convert character data to imem index int.")
-
-        if self.char_data != " " and self.flag_enc:
-            #print("Encountered Char:",self.char_data)
-            raise error.SyntaxError('Encountered "%s", Invalid Syntax.'% self.char_data)
-
-        if self.flag_decl:
-            check = [x for x in self.i if x == '"']
-
-            if len(check) == 1:
-                raise error.SyntaxError('Missing one " in order to declare Var')
-            elif len(check) > 2:
-                raise error.SyntaxError('too many " in line')
-
-            name = self.i.split('"')[1].split('"')[0]
-            if " " in name:
-                raise error.SyntaxError("More than one word found between the quotation marks.")
-            value = self.i[:-1].split('$')[1].split('~')[0]
-            self.var_list.append({name:value})
-            self.flag_decl = False
-            #print(self.var_list)
-            print('decl: "%s" created with the value "%s".'% (name,value))
-
     def flag_handler(self):
+
+        if self.char_data in (" ","i"): return 0
+
         if self.flag_const:
             i = 0
             while self.imem[i] != 0: i+=1
@@ -65,7 +38,7 @@ class interpreter:
             self.flag_const = False
             return 0
 
-        if self.flag_out:
+        elif self.flag_out:
             if self.flag_directout:
                 try: 
                     ifiltered = self.i[5:-1].split("~")[0].replace(" ","").replace("$","")
@@ -77,16 +50,18 @@ class interpreter:
                 self.flag_out = False
                 return 0
 
-            if self.flag_imemout:
+            elif self.flag_imemout:
                 try:
-                    i = self.imem[int(self.char_data.replace("~","").replace("i",""))]
+                    i = self.imem[int(self.char_data.replace("~",""))]
                 except IndexError:
                     raise IndexError("Invalid Imemory index.")
-                print('out: at index %s there is "%s".' % (self.char_data.replace("~",""), i))
+                ind = self.char_data.replace("~","").replace(" ","")
+                print('out: at index %s there is "%s".' % (ind, i))
+                self.flag_imemout
                 self.flag_out = False
-            return 0
+                return 0
 
-        if self.flag_aout:
+        elif self.flag_aout:
             if self.flag_directaout:
 
                 try: 
@@ -94,42 +69,72 @@ class interpreter:
                 except ValueError:
                     raise error.ValueError('Cannot convert "%s" to integer, is invalid.' % self.i[6:-1])
 
-                self.flag_directaout = False
                 self.flag_skip = True
                 while self.i[self.skipby] != "\n": self.skipby+=1
+                self.flag_directaout = False
                 self.flag_aout = False
                 return 0
 
-            self.flag_skip = True
-            self.skipby = 1
-            self.flag_aout = False
-            return 0
+            elif self.flag_imemaout:
 
-        if self.flag_enc: 
-            #print("Encountered Num:",self.char_data.split("~")[0])
-            raise error.SyntaxError('Encountered "%s", Invalid Syntax.' % self.char_data)
+                try:
+                    ind = int(self.i[6:-1].replace("i",""))
+                    value = int(self.imem[ind])
+                    print('aout: at index %s there is "%s".' % (ind, chr(value) ))
+                except(TypeError,ValueError):
+                    raise error.TypeError("integer argument expected, got float")
+
+                self.flag_skip = True
+                while self.i[self.skipby] != "\n": self.skipby+=1
+                self.flag_imemaout = False
+                self.flag_aout = False
+                return 0
+
+        elif self.flag_decl:
+            err = "raise error.SyntaxError('Invalid Syntax. Type for variable passed is invalid.')"
+            chk_num = lambda: "float" if self.i[5:11] == "float " else "int" if self.i[5:9] == "int " else exec(err)
+            chk = lambda: "str" if self.i[5:12] == "string " else "bool" if self.i[5:10] == "bool " else chk_num()
+
+            check = [x for x in self.i if x == '"']
+            name = self.i.split('"')[1].split('"')[0]
+            value = self.i[:-1].split('$')[1].split('~')[0]
+
+            if len(check) == 1:
+                raise error.SyntaxError('Missing one " in order to declare Var')
+            elif len(check) > 2:
+                raise error.SyntaxError('too many " in line')
+
+            if " " in name:
+                raise error.SyntaxError("More than one word found between the quotation marks.")
+
+            if value.lower() not in ("true","false","1","0") and chk() == "bool":
+                raise error.ValueError("Invalid Value %s cannot convert to boolean."%value)
+            else:
+                exec("%s('%s')"%(chk(),value))
+            
+            self.var_list.append( {name : (chk(),value)} )
+            self.flag_decl = False
+            print('decl: "%s" created with the type:"%s" and value "%s".'% (name,chk(),value))
 
     def encountered(self):
         try:
             float(self.char_data)
-            self.flag_skip = True
-        except ValueError:
-            self.char_data_handler()
-            return 0
+        except ValueError: return 0
+
         var1 = 1
-        while self.i[self.pos_char+var1] not in ("\n"):
-            self.char_data+=self.i[self.pos_char+var1]
+        while self.i[self.pos_char+var1] != "\n":
+            self.char_data += self.i[self.pos_char+var1]
 
             if "~" in self.char_data:
                 break
 
-            try: 
-                if self.flag_enc: float(self.char_data)
-            except ValueError:
-                raise error.ValueError("Cannot convert character data to Float")
+            if "01234567890" in self.char_data:
+                try: float(self.char_data)
+
+                except ValueError:
+                    raise error.ValueError("Cannot convert character data to Float")
+
             var1+=1
-        self.skipby += (var1-1)
-        self.flag_handler()
 
     def chars(self): #numbers, etc
         for self.pos_char,self.char_data in enumerate(self.i):
@@ -150,12 +155,9 @@ class interpreter:
             elif self.flag_skip and self.skipby == 0:
                 self.flag_skip = False
 
-            if self.flag_decl:
-                pass
-
             if not self.flag_aout:
                 self.encountered()
-            else: self.flag_handler()
+            self.flag_handler()
 
     def full_words(self): #keyword handler
 
@@ -164,40 +166,51 @@ class interpreter:
             # msvcrt.getch()
             sys.exit()
 
-        elif self.i[:5].upper() == "CONST":
+        elif self.i[:6].upper() == "CONST ":
             self.flag_const = True
             self.flag_skip = True
             self.skipby = 5
 
-        elif self.i[:3].upper() == "OUT":
+        elif self.i[:4].upper() == "OUT ":
             self.flag_out = True
             self.flag_skip = True
             self.skipby = 3
-            if "$" in self.i:
+
+            if "$" in self.i and "i" in self.i:
+                raise error.SyntaxError("Invalid Syntax. More than one special character present")
+
+            elif "$" in self.i:
                 self.flag_directout = True
                 self.skipby+=1
                 return 0
-            if "i" in self.i:
+            elif "i" in self.i:
                 self.flag_imemout = True
                 self.skipby+=1
                 return 0
             else:
                 raise error.SyntaxError("Invalid Syntax. No denomination character present.")
 
-        elif self.i[:4].upper() == "AOUT":
+        elif self.i[:5].upper() == "AOUT ":
             self.flag_aout = True
             self.flag_skip = True
             self.skipby = 4
-            if "$" in self.i[4:]:
+            if "$" in self.i and "i" in self.i:
+                raise error.SyntaxError("Invalid Syntax. More than one special character present")
+            elif "$" in self.i:
                 self.flag_directaout = True
                 self.skipby+=1
+            elif "i" in self.i:
+                self.flag_imemaout = True
+                self.skipby+=1
+                return 0
+            else:
+                raise error.SyntaxError("Invalid Syntax. No denomination character present.")
 
-        elif self.i[:4].upper() == "DECL":
+        elif self.i[:5].upper() == "DECL:":
             self.flag_skip = True
             self.skipby = 5
-            if '"' in self.i[4:]:
+            if '"' in self.i:
                 self.flag_decl = True
-                self.flag_enc = False
             else:
                 raise error.SyntaxError('Missing both " in order to declare Var')
 
@@ -215,9 +228,6 @@ class interpreter:
             if self.flag_start:
                 self.full_words()
                 self.chars()
-
-            if not self.flag_enc:
-                self.flag_enc = True
 
 dfile = "./runf.toye"
 
