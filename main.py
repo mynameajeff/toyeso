136d07ff43e9aedad8b111b640526690ec9b5e6c
mynameajeff <yungcoder@protonmail.com>
2017-07-14 23:30:06 +0100

    Update main.py
    Rewrote decl code, changed the way the commands print out(removed debug messages)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, msvcrt, error
+import sys, error
 
 class interpreter:
     def __init__(self, fl):
@@ -6,6 +6,7 @@ class interpreter:
         self.var_list = []
 
         self.flag_start = None
+        self.flag_exit = None
 
         self.flag_const = None
         self.flag_decl = None
@@ -37,64 +38,13 @@ class interpreter:
             i = 0
             while self.imem[i] != 0: i+=1
             self.imem[i] = self.char_data.replace(" ","")
-            print("const: stored constant %s at index %s." % (self.imem[i], i))
+            #print("const: stored constant %s at index %s." % (self.imem[i], i))
             self.flag_const = False
-
-        elif self.flag_out:
-            if self.flag_directout:
-                try: 
-                    ifiltered = self.i[5:-1].replace("$","").rstrip()
-                    float(ifiltered)
-                    print('out: input was "%s"' % ifiltered)
-                except ValueError:
-                    raise error.TypeError(error.Conv_Float % ifiltered)
-
-                self.flag_directout = False
-                self.flag_out = False
-
-            elif self.flag_imemout:
-                try:
-                    iz = self.char_data.replace("i","").rstrip()
-                    i = self.imem[int(iz)]
-                except IndexError:
-                    raise error.IndexError(error.Mem_Index)
-                print('out: at index %s there is "%s".' % (iz, i))
-                self.flag_imemout
-                self.flag_out = False
-
-        elif self.flag_aout:
-            if self.flag_directaout:
-
-                try: 
-                    ifiltered = self.i[6:-1].replace("$","").rstrip()
-                    print('aout: %s' % chr(int(ifiltered)))
-                except ValueError:
-                    raise error.TypeError(error.Conv_Int % ifiltered)
-
-                self.flag_skip = True
-                while self.i[self.skipby] != "\n": self.skipby+=1
-                self.flag_directaout = False
-                self.flag_aout = False
-
-            elif self.flag_imemaout:
-
-                try:
-                    ind = int(self.i[6:-1].replace("i",""))
-                    value = int(self.imem[ind])
-                    print('aout: at index %s there is "%s".' % 
-                        (ind, chr(value)))
-                except (TypeError,ValueError):
-                    raise error.TypeError(error.Int_exp_got_float)
-
-                self.flag_skip = True
-                while self.i[self.skipby] != "\n": self.skipby+=1
-                self.flag_imemaout = False
-                self.flag_aout = False
 
         elif self.flag_decl:
 
             check = len([x for x in self.i if x == '"'])
-            name = self.i.split('"')[1].split('"')[0]
+            name = self.i.split('"')[1]
             value = self.i[:-1].split('$')[1].rstrip()
 
             if check == 1:
@@ -115,8 +65,60 @@ class interpreter:
             
             self.var_list.append( {name : (chk(self.i),value)} )
             self.flag_decl = False
-            print('decl: "%s" created with the type:"%s" and value "%s".' % 
-                (name,chk(self.i),value))
+            #print('decl: "%s" created with the type:"%s" and value "%s".' % 
+            #    (name,chk(self.i),value))
+
+        elif self.flag_out:
+            if self.flag_directout:
+                try: 
+                    ifiltered = self.i[5:-1].replace("$","").rstrip()
+                    print('%.f' % float(ifiltered), end = "")
+                except ValueError:
+                    raise error.TypeError(error.Conv_Float % ifiltered)
+
+                self.flag_directout = False
+                self.flag_out = False
+
+            elif self.flag_imemout:
+                try:
+                    iz = self.char_data.replace("i","").rstrip()
+                    print('%s' % self.imem[int(iz)], end = "")
+                except IndexError:
+                    raise error.IndexError(error.Mem_Index)
+                self.flag_imemout
+                self.flag_out = False
+
+        elif self.flag_aout:
+            if self.flag_directaout:
+
+                try: 
+                    ifiltered = self.i[6:-1].replace("$","")
+                    print('%s' % chr(int(ifiltered.rstrip())), end = "")
+                except ValueError:
+                    raise error.TypeError(error.Conv_Int % ifiltered)
+
+                self.flag_skip = True
+                try: 
+                    while self.i[self.skipby] != "\n": self.skipby+=1
+                except: pass
+                self.flag_directaout = False
+                self.flag_aout = False
+
+            elif self.flag_imemaout:
+
+                try:
+                    ind = int(self.i[6:].replace("i","").replace("\n",""))
+                    value = int(self.imem[ind])
+                    print('%s' % chr(value), end = "")
+                except (TypeError,ValueError):
+                    raise error.TypeError(error.Int_exp_got_float)
+
+                self.flag_skip = True
+                try:
+                    while self.i[self.skipby] != "\n": self.skipby+=1
+                except: pass
+                self.flag_imemaout = False
+                self.flag_aout = False
 
     def encountered(self):
 
@@ -136,6 +138,13 @@ class interpreter:
             var1+=1
 
     def chars(self): #numbers, etc
+
+        if not self.flag_active and self.flag_start:
+            if self.i[:3].upper() != 'END':
+                if self.i.replace(" ","") != '\n':
+                    isvar = self.i.replace("\n","")
+                    raise error.SyntaxError(error.NO_kw_present % isvar)
+
         for self.pos_char,self.char_data in enumerate(self.i):
 
             if self.char_data == '\n':
@@ -154,21 +163,28 @@ class interpreter:
             if self.char_data not in (" ","i"):
                 self.flag_handler()
 
-        if not self.flag_active and self.flag_start:
-            if self.i.replace(" ","") != '\n' and self.x < len(self.ifln)-1:
-                raise error.SyntaxError(error.NO_kw_present % self.i[:-1])
-
     def full_words(self): #keyword handler
 
         if self.i[:3].upper() == 'END':
             print("ending...")
-            sys.exit()
+            self.flag_exit = True
+            self.flag_skip = True
+            self.skipby = len(self.i)
 
         elif self.i[:6].upper() == "CONST ":
             self.flag_active+=1
             self.flag_const = True
             self.flag_skip = True
             self.skipby = 5
+
+        elif self.i[:5].upper() == "DECL:":
+            self.flag_active+=1
+            self.flag_skip = True
+            self.skipby = 5
+            if '"' in self.i:
+                self.flag_decl = True
+            else:
+                raise error.SyntaxError(error.missing_BOTH_quot)
 
         elif self.i[:4].upper() == "OUT ":
             self.flag_active+=1
@@ -207,17 +223,10 @@ class interpreter:
             else:
                 raise error.SyntaxError(error.NO_sc_present)
 
-        elif self.i[:5].upper() == "DECL:":
-            self.flag_active+=1
-            self.flag_skip = True
-            self.skipby = 5
-            if '"' in self.i:
-                self.flag_decl = True
-            else:
-                raise error.SyntaxError(error.missing_BOTH_quot)
-
     def main_loop(self):
         for self.x, self.i in enumerate(self.ifln):
+            if self.flag_exit:
+                sys.exit()
 
             if self.i[:5].upper() == 'START':
                 if self.flag_start:
@@ -225,7 +234,7 @@ class interpreter:
                 print("starting...")
                 self.flag_start = True
                 self.flag_skip = True
-                self.skipby = 5
+                self.skipby = len(self.i)
                 continue
 
             if self.flag_start:
@@ -234,6 +243,9 @@ class interpreter:
 
             if self.flag_active:
                 self.flag_active = 0
+
+        if not self.flag_exit:
+            raise error.SyntaxError(error.NO_end_present)
 
 def chk(i):
     if i[5:9] == "int ": return "int"
