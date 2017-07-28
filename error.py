8c093921f0e22381dc0c35e59e919777d8373719
mynameajeff <yungcoder@protonmail.com>
2017-07-28 22:11:56 +0100

    Update error.py

--- error.py
+++ error.py
@@ -1,47 +1,51 @@
 class SyntaxError(Exception):
     def __init__(self, message):
-        super().__init__("Invalid Syntax. %s" % message) 
+        super().__init__('Invalid Syntax. %s.' % message) 
 class ValueError(Exception):
     def __init__(self, message):
-        super().__init__("Invalid Values. %s" % message)
+        super().__init__('Invalid Values. %s.' % message)
 class IndexError(Exception):
     def __init__(self, message):
-        super().__init__("Invalid Index. %s" % message)
+        super().__init__('Invalid Index. %s.' % message)
 class TypeError(Exception):
     def __init__(self, message):
-        super().__init__("Invalid Type. %s" % message)
+        super().__init__('Invalid Type. %s.' % message)
 
 #~ Variable Presence Errors ~
-MTO_var_w_name = 'More than one Variable assigned the name "%s".'
-NO_var_w_name = 'No Variable assigned the name "%s".'
+MTO_var_w_name = 'More than one Variable assigned the name "%s", line %s'
+NO_var_w_name = 'No Variable assigned the name "%s", line %s'
 
-#~ Character Presence Errors ~
-MTO_lsquareb_present = "More than one [ is present."
-MTO_rsquareb_present = "More than one ] is present."
-MTO_st_present = "More than one START statement is present."
-NO_end_present = "No END statement is present."
-MTO_sc_present = "More than one special character present."
-NO_sc_present = "No denomination character present."
+#~ Character/Keyword Presence Errors ~
+NO_if_present = 'No IF statement is present, line %s'
+MTO_lsquareb_present = 'More than one [ is present, line %s'
+MTO_rsquareb_present = 'More than one ] is present, line %s'
+NO_start_present = 'No START statement is present, line %s'
+MTO_st_present = 'More than one START statement is present, line %s'
+NO_end_present = 'No END statement is present, line %s'
+MTO_sc_present = 'More than one special character present, line %s'
+NO_sc_present = 'No denomination character present, line %s'
+NO_kw_present = 'No keyword given for data: "%s", line %s'
 
 #~ Quotation Errors ~
-missing_square_brackets = "Missing both [] in order to form logical check."
-missing_lsquare_bracket = "Missing [ in order to form logical check."
-missing_rsquare_bracket = "Missing ] in order to form logical check."
-bsquareb_MTO_present = "More than one ] and [ is present."
-missing_BOTH_quot = 'Missing both " in order to declare the variable.'
-missing_ONE_quot = 'Missing one " in order to declare the variable.'
-too_many_quot = 'Too many " in line, cannot declare the variable.'
-MTO_word_in_quot = 'More than one word found in quotation marks.'
-
-#~ Keyword Errors ~
-NO_kw_present = 'No keyword given for data: "%s".'
+missing_square_brackets = 'Missing both [] in order to form logical check, line %s'
+missing_square_bracket = 'Missing "%s" in order to form logical check, line %s'
+bsquareb_MTO_present = 'More than one ] and [ is present, line %s'
+missing_BOTH_quot = "Missing both ' in order to declare the variable, line %s"
+missing_ONE_quot = "Missing one ' in order to declare the variable, line %s"
+too_many_quot = 'Too many " in line, cannot declare the variable, line %s'
+MTO_word_in_quot = 'More than one word found in quotation marks, line %s'
 
 #~ Type Errors ~
-Int_exp_got_float = 'Integer was expected, got Float instead.'
-Conv_Float = 'Cannot convert character data: "%s" to Float.'
-Conv_Int = 'Cannot convert character data: "%s" to Integer.'
-Conv_Bool = 'Cannot convert character data: "%s" to Boolean.'
-Conv_Type = 'Type for variable passed is invalid.'
+Int_exp_got_float = 'Integer was expected, got Float instead, line %s'
+Conv_Float = 'Cannot convert character data: "%s" to Float, line %s'
+Conv_Int = 'Cannot convert character data: "%s" to Integer, line %s'
+Conv_Bool = 'Cannot convert character data: "%s" to Boolean, line %s'
+Conv_Type = 'Type for variable passed is invalid, line %s'
 
 #~ Index Errors ~
-Mem_Index = 'Invalid Memory index present.'
+Mem_Index = 'Invalid Memory index present'
+
+#~ Logical Errors ~
+NO_Logic_present = 'No logical statement present between square brackets, line %s'
+Inv_Op_present = 'Invalid Operator present within logical expression, line %s'
+Inv_Args_present = 'Invalid Amount of Expression arguments present, line %s'
